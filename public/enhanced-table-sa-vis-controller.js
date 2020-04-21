36255e95a42a4cbd1c75ea53c85ebeb38f3189b5
nsheo <nsheo@ntels.com>
2020-04-22 08:55:09 +0900

    edit audio path

--- public/enhanced-table-sa-vis-controller.js
+++ public/enhanced-table-sa-vis-controller.js
@@ -976,7 +976,7 @@ function EnhancedTableSaVisController ($scope, Private, config) {
 				if(alarmCheck){
 					//runAudioRedAlert();
 					console.log("Check test buzz")
-					var alarm = new buzz.sound("./alert_red.wav");
+					var alarm = new buzz.sound("audio/alert_red.wav");
 					console.log(alarm);
 					alarm.play();
 				}
