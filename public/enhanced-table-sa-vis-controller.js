2ae59e59e41e8b0af278188bdc61bbc8a6fd2901
nsheo <nsheo@ntels.com>
2020-04-22 10:25:31 +0900

    test

--- public/enhanced-table-sa-vis-controller.js
+++ public/enhanced-table-sa-vis-controller.js
@@ -975,13 +975,7 @@ function EnhancedTableSaVisController ($scope, Private, config) {
 				
 				if(alarmCheck){
 					//runAudioRedAlert();
-					console.log("Check test buzz")
-					var alarm = new buzz.sound("/audio/alert_red.wav", {
-										crossOrigin: true
-									});
-					alarm.load();
-					console.log(alarm);					
-					alarm.play();
+					console.log("Check check Alarm buzz")
 				}
 			});
         }
